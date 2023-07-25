e751287291fc889a8942e2b49e7bf83f34b4332d
KramYengir <kramyengir@gmail.com>
2023-07-25 14:34:59 +0100

    fill out & style welcome section

--- src/modules/homepage.js
+++ src/modules/homepage.js
@@ -1,12 +1,28 @@
 import '../styles/homepage.scss';
 
+const TITLE = 'Welcome to Ferryhill';
+const MSG = "Voted Salford's Best Chippy 2023 by SalfordNow.co.uk readers";
+
 const buildHomepage = (()=>{
     const container = document.createElement('div');
     container.id = 'homepage-container';
    
+    //welcome section
     const welcomeDiv = document.createElement('div');
     welcomeDiv.id = 'welcome';
 
+    const welcomeTitle = document.createElement('div');
+    welcomeTitle.innerText = TITLE;
+    welcomeTitle.id = 'welcome-title';
+    
+    const welcomeMsg = document.createElement('div');
+    welcomeMsg.innerText = MSG;
+    welcomeMsg.id = 'welcome-msg';
+
+    welcomeDiv.appendChild(welcomeTitle);
+    welcomeDiv.appendChild(welcomeMsg);
+
+    //news section
     const newsDiv = document.createElement('div');
     newsDiv.id = 'news';
 
