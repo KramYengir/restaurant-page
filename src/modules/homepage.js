f711c6bbb65afde20e99e49c6edb85459decb8a6
KramYengir <kramyengir@gmail.com>
2023-07-25 15:41:01 +0100

    fill out hours section

--- src/modules/homepage.js
+++ src/modules/homepage.js
@@ -1,7 +1,20 @@
 import '../styles/homepage.scss';
 
-const TITLE = 'Welcome to Ferryhill';
-const MSG = "Voted Salford's Best Chippy 2023 by SalfordNow.co.uk readers";
+const WELCOME_TITLE = 'Welcome to Ferryhill';
+const WELCOME_MSG = "Voted Salford's Best Chippy 2023 by SalfordNow.co.uk readers";
+const HOURS_MSG = 'Be sure to check our social media pages or get in touch with us for up-to-date changes';
+const HOURS_INFO = `<div class='heading' id='title'>Opening Hours</div>
+                    <div class='heading days'>Mon - Sat</div>
+                    <div class='times'>11:30am  -  1:45pm</div>
+                    <div class='times'>4:30pm  -  8:30pm</div>
+                    <div class='heading days'>Fri</div>
+                    <div class='times'>11:30am  -  2:00pm</div>
+                    <div class='times'>4:30pm  -  8:30pm</div>
+                    <div class='heading days'>Sunday</div>
+                    <div class='times'>--closed--</div>
+                    `
+                    ;
+
 
 const buildHomepage = (()=>{
     const container = document.createElement('div');
@@ -12,25 +25,33 @@ const buildHomepage = (()=>{
     welcomeDiv.id = 'welcome';
 
     const welcomeTitle = document.createElement('div');
-    welcomeTitle.innerText = TITLE;
+    welcomeTitle.innerText = WELCOME_TITLE;
     welcomeTitle.id = 'welcome-title';
     
     const welcomeMsg = document.createElement('div');
-    welcomeMsg.innerText = MSG;
+    welcomeMsg.innerText = WELCOME_MSG;
     welcomeMsg.id = 'welcome-msg';
 
     welcomeDiv.appendChild(welcomeTitle);
     welcomeDiv.appendChild(welcomeMsg);
 
-    //news section
-    const newsDiv = document.createElement('div');
-    newsDiv.id = 'news';
-
+    //opening hours section
     const openingHoursDiv = document.createElement('div');
     openingHoursDiv.id = 'hours';
 
+    const hoursMsg = document.createElement('div');
+    hoursMsg.id = 'hours-msg';
+    hoursMsg.innerText = HOURS_MSG;
+
+    const hoursInfo = document.createElement('div');
+    hoursInfo.id = 'hours-info';
+    hoursInfo.innerHTML = HOURS_INFO;
+
+    openingHoursDiv.appendChild(hoursMsg);
+    openingHoursDiv.appendChild(hoursInfo);
+
+
     container.appendChild(welcomeDiv);
-    container.appendChild(newsDiv);
     container.appendChild(openingHoursDiv);
 
     return container;
