323fc4a30b021d35e820c1178dc029b8dc4b9dab
KramYengir <kramyengir@gmail.com>
2023-08-21 14:28:51 +0100

    add a img & text box

--- src/modules/about.js
+++ src/modules/about.js
@@ -1,8 +1,33 @@
 import '../styles/about.scss';
 
 const buildAboutpage = (()=>{
+
+    const textContent = [
+        'Ferryhill, what a place to be, bal blab blabalb',
+        'hjgfj sdgf jgsd fgs dhgfjgs djf g hds gfjgd hddfsg jdhg f',
+        'hjgfj sdgf jgsd fgs dhgfjgs djf g hds gfjgd hddfsg jdhg f',
+        'hjgfj sdgf jgsd fgs dhgfjgs djf g hds gfjgd hddfsg jdhg f',    
+    ]
+
     const container = document.createElement('div');
     container.id = 'about-container';
+
+    
+    for(let i = 1; i<4; i++){
+
+        let imgContainer = document.createElement('div');
+        imgContainer.classList.add('img-container');
+        imgContainer.id = `img-${i}`;
+
+        let textContainer = document.createElement('div');
+        textContainer.classList.add('text-container');
+        textContainer.id = `text-${i}`;
+        textContainer.textContent = `${textContent[i-1]}`;
+
+        container.appendChild(imgContainer);
+        container.appendChild(textContainer);
+        
+    }
 
 
     return container;
