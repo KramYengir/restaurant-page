28b530c9ba6907f41b02f46ad0f3fcbb1f5c8341
KramYengir <kramyengir@gmail.com>
2023-11-03 18:25:08 +0000

    update about us content

--- src/modules/about.js
+++ src/modules/about.js
@@ -3,9 +3,16 @@ import '../styles/about.scss';
 const buildAboutpage = (()=>{
 
     const textContent = [
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque finibus ultrices enim sed ornare. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quis posuere orci.',
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque finibus ultrices enim sed ornare. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quis posuere orci.',
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque finibus ultrices enim sed ornare. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quis posuere orci.',
+        `Ferryhill Fish and Chips is a family-run business established in 
+        2013 by husband and wife, Mike and Amelia.
+        With over 30 years working within the industry, 
+        we bring our knowledge and passion to provide quality food and 
+        good friendly service to our customers.`,
+        `Under the stewardship of nephew Christos since 2020, our shop has flourished, 
+        garnering consistent 5-star reviews on Google. 
+        Our menu continues to evolve, and we are proud to have been voted 
+        the best chippy in Manchester by readers of salfordnow.co.uk in 2023.`,
+        `Under Christos's leadership, the menu has expanded to include a diverse selection of vegan, vegetarian, and gluten-free options, catering to a wide range of dietary preferences.`,
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque finibus ultrices enim sed ornare. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quis posuere orci.'
     ]
 
