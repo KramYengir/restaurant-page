88f8aaa2ac58f7aa77c0623493c67898bd29160d
KramYengir <kramyengir@gmail.com>
2023-11-10 14:56:12 +0000

    implement slider & prettified some files

--- src/modules/homepage.js
+++ src/modules/homepage.js
@@ -1,8 +1,12 @@
-import '../styles/homepage.scss';
+import "../styles/homepage.scss";
 
-const WELCOME_TITLE = 'Welcome to Ferryhill!';
-const WELCOME_MSG = "Voted Salford's Best Chippy in 2023 by Salfordnow.co.uk readers";
-const HOURS_MSG = 'Be sure to check our social media pages or get in touch with us for up-to-date changes';
+import Slider from "./slider";
+
+const WELCOME_TITLE = "Welcome to Ferryhill!";
+const WELCOME_MSG =
+  "Voted Salford's Best Chippy in 2023 by Salfordnow.co.uk readers";
+const HOURS_MSG =
+  "Be sure to check our social media pages or get in touch with us for up-to-date changes";
 const HOURS_INFO = `<div class='heading' id='title'>Opening Hours</div>
                     <div class='heading days'>Mon - Sat</div>
                     <div class='times'>11:30am  -  1:45pm</div>
@@ -12,56 +16,51 @@ const HOURS_INFO = `<div class='heading' id='title'>Opening Hours</div>
                     <div class='times'>4:30pm  -  8:30pm</div>
                     <div class='heading days'>Sunday</div>
                     <div class='times'>--closed--</div>
-                    `
-                    ;
+                    `;
+const buildHomepage = (() => {
+  const container = document.createElement("div");
+  container.id = "homepage-container";
 
+  //welcome section
+  const welcomeDiv = document.createElement("div");
+  welcomeDiv.id = "welcome";
 
-const buildHomepage = (()=>{
-    const container = document.createElement('div');
-    container.id = 'homepage-container';
-   
-    //welcome section
-    const welcomeDiv = document.createElement('div');
-    welcomeDiv.id = 'welcome';
+  const welcomeTitle = document.createElement("div");
+  welcomeTitle.innerText = WELCOME_TITLE;
+  welcomeTitle.id = "welcome-title";
 
-    const welcomeTitle = document.createElement('div');
-    welcomeTitle.innerText = WELCOME_TITLE;
-    welcomeTitle.id = 'welcome-title';
-    
-    const welcomeMsg = document.createElement('div');
-    welcomeMsg.innerText = WELCOME_MSG;
-    welcomeMsg.id = 'welcome-msg';
+  const welcomeMsg = document.createElement("div");
+  welcomeMsg.innerText = WELCOME_MSG;
+  welcomeMsg.id = "welcome-msg";
 
-    welcomeDiv.appendChild(welcomeTitle);
-    welcomeDiv.appendChild(welcomeMsg);
+  welcomeDiv.appendChild(welcomeTitle);
+  welcomeDiv.appendChild(welcomeMsg);
 
-    //opening hours section
-    const openingHoursDiv = document.createElement('div');
-    openingHoursDiv.id = 'hours';
+  //opening hours section
+  const openingHoursDiv = document.createElement("div");
+  openingHoursDiv.id = "hours";
 
-    const hoursMsg = document.createElement('div');
-    hoursMsg.id = 'hours-msg';
-    hoursMsg.innerText = HOURS_MSG;
+  const hoursMsg = document.createElement("div");
+  hoursMsg.id = "hours-msg";
+  hoursMsg.innerText = HOURS_MSG;
 
-    const hoursDiv = document.createElement('div');
-    hoursDiv.id = 'hours-div';
+  const hoursDiv = document.createElement("div");
+  hoursDiv.id = "hours-div";
 
+  const hoursInfo = document.createElement("div");
+  hoursInfo.id = "hours-info";
+  hoursInfo.innerHTML = HOURS_INFO;
 
-    const hoursInfo = document.createElement('div');
-    hoursInfo.id = 'hours-info';
-    hoursInfo.innerHTML = HOURS_INFO;
+  hoursDiv.appendChild(hoursInfo);
 
-    hoursDiv.appendChild(hoursInfo);
+  openingHoursDiv.appendChild(hoursMsg);
+  openingHoursDiv.appendChild(hoursDiv);
 
-    openingHoursDiv.appendChild(hoursMsg);
-    openingHoursDiv.appendChild(hoursDiv);
+  container.appendChild(welcomeDiv);
+  container.appendChild(Slider);
+  container.appendChild(openingHoursDiv);
 
-
-    container.appendChild(welcomeDiv);
-    container.appendChild(openingHoursDiv);
-
-    return container;
-
+  return container;
 })();
 
 export default buildHomepage;
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
     clean: true,
-    assetModuleFilename: '[name][ext]'
+    assetModuleFilename: "[name][ext]",
   },
-  devtool: 'source-map',
+  devtool: "source-map",
   devServer: {
     static: {
-        directory: path.resolve(__dirname, 'dist')
+      directory: path.resolve(__dirname, "dist"),
     },
     open: true,
     hot: true,
@@ -21,26 +21,26 @@ module.exports = {
     rules: [
       {
         test: /\.s?css$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
-        type: 'asset/resource'
+        type: "asset/resource",
       },
-      {
+      /* {
         test: /\.html$/,
         use: 'html-loader',
         
-      }
+      } */
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-        title: 'Halloumi Gold',
-        filename: 'index.html',
-        template: 'src/template.html',
-        favicon: 'src/assets/favicon.ico',
-    })
+      title: "Halloumi Gold",
+      filename: "index.html",
+      template: "src/template.html",
+      favicon: "src/assets/favicon.ico",
+    }),
   ],
-  mode: 'development'
+  mode: "development",
 };
