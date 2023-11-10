05640fe6acc39cb53074b4b105dfdc86791cb955
KramYengir <kramyengir@gmail.com>
2023-11-10 12:34:56 +0000

    add video asset

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,11 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
         type: 'asset/resource'
+      },
+      {
+        test: /\.html$/,
+        use: 'html-loader',
+        
       }
     ],
   },
