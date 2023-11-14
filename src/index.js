44b0bab7c880d8ab83362038f9d325f092137590
KramYengir <kramyengir@gmail.com>
2023-11-14 16:40:56 +0000

    stay on current page after reloads

--- src/index.js
+++ src/index.js
@@ -1,51 +1,59 @@
-import './styles/main.scss';
-import header from './modules/header.js';
-import homepage from './modules/homepage.js';
-import menu from './modules/menu.js';
-import contact from './modules/contact.js';
-import about from './modules/about.js';
-import footer from './modules/footer.js';
-
-
-let currentPage = homepage;
+import "./styles/main.scss";
+import header from "./modules/header.js";
+import homepage from "./modules/homepage.js";
+import menu from "./modules/menu.js";
+import contact from "./modules/contact.js";
+import about from "./modules/about.js";
+import footer from "./modules/footer.js";
 
 const pages = [homepage, menu, about, contact];
 
-const content = document.getElementById('content');
-const contentMain = document.createElement('div');
-contentMain.id = 'content-main';
-contentMain.appendChild(currentPage);
+const content = document.getElementById("content");
+const contentMain = document.createElement("div");
+let currentPage = homepage;
 
+// keep current page after reload
+let lastPageIndex = sessionStorage.getItem("lastPageIndex");
+
+if (lastPageIndex) {
+  currentPage = pages[lastPageIndex];
+}
+
+contentMain.id = "content-main";
+contentMain.appendChild(currentPage);
 content.appendChild(header);
 content.appendChild(contentMain);
 content.appendChild(footer);
 
-const tabLinks = document.querySelectorAll('a');
+const tabLinks = document.querySelectorAll("a");
 
-const loadContent = (index)=>{
-    console.log(index);
-    contentMain.removeChild(currentPage);
-    contentMain.appendChild(pages[index]);
-    currentPage = pages[index];
-}
+const loadContent = (index) => {
+  contentMain.removeChild(currentPage);
+  contentMain.appendChild(pages[index]);
+  currentPage = pages[index];
+  lastPageIndex = index;
+  sessionStorage.setItem("lastPageIndex", index);
+};
 
-const toggleActiveTab = (index)=>{
-    tabLinks.forEach((el,i)=>{
-        if(i === index){
-            tabLinks[index].classList.add('active')
-        }
-        else{
-            tabLinks[i].classList.remove('active')
-            console.log('matching index');
-        }
-    })
-}
+const toggleActiveTab = (index) => {
+  tabLinks.forEach((el, i) => {
+    if (i === index) {
+      tabLinks[index].classList.add("active");
+    } else {
+      tabLinks[i].classList.remove("active");
+      console.log("matching index");
+    }
+  });
+};
 
-tabLinks.forEach((el, i) =>{
-    el.addEventListener('click', ()=>{
-        loadContent(el.dataset.index);
-        toggleActiveTab(i);
-    })
-})
+tabLinks.forEach((el, i) => {
+  el.addEventListener("click", () => {
+    loadContent(el.dataset.index);
+    toggleActiveTab(i);
+  });
+});
 
-
+window.addEventListener("beforeunload", () => {
+  window.scrollTo(0, 0);
+  loadContent(lastPageIndex);
+});
